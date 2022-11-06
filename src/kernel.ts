2cbb1483e091d88dd94e81dbe2691151851998e0
Yuta Saito <kateinoigakukun@gmail.com>
2022-11-06 08:31:40 +0000

    Allow to load bundler

--- src/kernel.ts
+++ src/kernel.ts
@@ -2,24 +2,134 @@ import { KernelMessage } from '@jupyterlab/services';
 
 import { BaseKernel } from '@jupyterlite/kernel';
 import { RbValue, RubyVM } from 'ruby-head-wasm-wasi';
-import { DefaultRubyVM } from 'ruby-head-wasm-wasi/dist/browser';
+import { WASI } from "@wasmer/wasi";
+import { WasmFs } from "@wasmer/wasmfs";
+// @ts-ignore
+import * as path from "path-browserify";
+
+interface IOWriter {
+  write(line: string, type: "stdout" | "stderr"): void;
+}
+
+class ConsoleWriter implements IOWriter {
+  write(line: string, type: "stdout" | "stderr") {
+    if (type === "stdout") {
+      console.log(line);
+    } else {
+      console.error(line);
+    }
+  }
+}
 
 /**
  * A kernel that interpret Ruby code.
  */
 export class CRubyKernel extends BaseKernel {
   private _vmPromise: Promise<{ vm: RubyVM; mainBind: RbValue }> | null = null;
+  private _ioWriter: IOWriter = new ConsoleWriter();
+
   async useVM(): Promise<{ vm: RubyVM; mainBind: RbValue }> {
     if (this._vmPromise) {
       return this._vmPromise;
     }
     this._vmPromise = (async () => {
+      const wasmFs = new WasmFs();
+
+      const originalWriteSync = wasmFs.fs.writeSync.bind(wasmFs.fs);
+      const stdOutErrBuffers: { [key: number]: string } = { 1: "", 2: "" };
+      const self = this;
+      wasmFs.fs.writeSync = function () {
+          let fd: number = arguments[0];
+          let text;
+          if (arguments.length === 4) {
+              text = arguments[1];
+          }
+          else {
+              let buffer = arguments[1];
+              text = new TextDecoder("utf-8").decode(buffer);
+          }
+          const handlers: { [key: number]: (text: string) => void } = {
+              1: (line: string) => self._ioWriter.write(line, "stdout"),
+              2: (line: string) => self._ioWriter.write(line, "stderr"),
+          };
+          if (handlers[fd]) {
+              text = stdOutErrBuffers[fd] + text;
+              let i = text.lastIndexOf("\n");
+              if (i >= 0) {
+                  handlers[fd](text.substring(0, i + 1));
+                  text = text.substring(i + 1);
+              }
+              stdOutErrBuffers[fd] = text;
+          }
+          // @ts-ignore
+          return originalWriteSync(...arguments);
+      };
+
+      const args = [
+        "ruby.wasm", "-e_=0", "-I/gems/lib"
+      ];
+
+      const vm = new RubyVM();
+      Error.stackTraceLimit = Infinity;
+      wasmFs.fs.mkdirSync("/home/me", { mode: 0o777, recursive: true });
+      wasmFs.fs.mkdirSync("/home/me/.gem/specs", { mode: 0o777, recursive: true });
+      wasmFs.fs.writeFileSync("/dev/null", new Uint8Array(0));
+      wasmFs.fs.mkdirSync("/gems/lib", { mode: 0o777, recursive: true });
+      wasmFs.fs.writeFileSync("/gems/lib/socket.rb", new Uint8Array(0));
+      const wasi = new WASI({
+        args,
+        env: {
+          "GEM_PATH": "/gems:/home/me/.gem/ruby/3.2.0+2",
+          "GEM_SPEC_CACHE": "/home/me/.gem/specs",
+          "RUBY_FIBER_MACHINE_STACK_SIZE": String(1024 * 1024 * 20),
+        },
+        preopens: {
+          "/home": "/home",
+          "/dev": "/dev",
+          "/gems": "/gems",
+        },
+        bindings: {
+          ...WASI.defaultBindings,
+          fs: wasmFs.fs,
+          path: path,
+        }
+      });
+
+      const wrapWASI = (wasiObject: any) => {
+        // PATCH: @wasmer-js/wasi@0.x forgets to call `refreshMemory` in `clock_res_get`,
+        // which writes its result to memory view. Without the refresh the memory view,
+        // it accesses a detached array buffer if the memory is grown by malloc.
+        // But they wasmer team discarded the 0.x codebase at all and replaced it with
+        // a new implementation written in Rust. The new version 1.x is really unstable
+        // and not production-ready as far as katei investigated in Apr 2022.
+        // So override the broken implementation of `clock_res_get` here instead of
+        // fixing the wasi polyfill.
+        // Reference: https://github.com/wasmerio/wasmer-js/blob/55fa8c17c56348c312a8bd23c69054b1aa633891/packages/wasi/src/index.ts#L557
+        const original_clock_res_get = wasiObject.wasiImport["clock_res_get"];
+        wasiObject.wasiImport["clock_res_get"] = function () {
+          wasiObject.refreshMemory();
+          return original_clock_res_get(...arguments)
+        };
+        wasiObject.wasiImport["fd_fdstat_set_flags"] = function () {
+          return 0;
+        };
+        return wasiObject.wasiImport;
+      }
+
+      const imports = {
+        wasi_snapshot_preview1: wrapWASI(wasi),
+      }
+      vm.addToImports(imports)
       const response = await fetch(
-        'https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@0.3.0-2022-10-17-a/dist/ruby+stdlib.wasm'
+        'https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@0.4.0-2022-11-05-b/dist/ruby+stdlib.wasm'
       );
       const buffer = await response.arrayBuffer();
-      const module = await WebAssembly.compile(buffer);
-      const { vm } = await DefaultRubyVM(module);
+      const { instance } = await WebAssembly.instantiate(buffer, imports);
+      await vm.setInstance(instance);
+
+      wasi.setMemory(instance.exports.memory as WebAssembly.Memory);
+      (instance.exports._initialize as Function)();
+      vm.initialize(args);
       return { vm, mainBind: vm.eval('binding') };
     })();
     return this._vmPromise;
@@ -56,6 +166,16 @@ export class CRubyKernel extends BaseKernel {
     return content;
   }
 
+  async withWriter<R>(writer: IOWriter, body: () => Promise<R>): Promise<R> {
+    const oldWriter = this._ioWriter;
+    this._ioWriter = writer;
+    try {
+      const result = await body();
+      return result;
+    } finally {
+      this._ioWriter = oldWriter;
+    }
+  }
   /**
    * Handle an `execute_request` message
    *
@@ -66,9 +186,18 @@ export class CRubyKernel extends BaseKernel {
   ): Promise<KernelMessage.IExecuteReplyMsg['content']> {
     const { code } = content;
     try {
-      const { vm, mainBind } = await this.useVM();
-
-      const result = mainBind.call('eval', vm.wrap(code).call('to_s'));
+      const { vm } = await this.useVM();
+      (globalThis as any).RUBY_TMP_CODE = code;
+      const writer: IOWriter = {
+        write: (text, type) => {
+          this.stream({ name: type, text });
+        }
+      };
+      const result = await this.withWriter(writer, async () => {
+        return await vm.evalAsync(`
+          Kernel.eval(JS.global[:RUBY_TMP_CODE].to_s, TOPLEVEL_BINDING)
+        `);
+      })
       this.publishExecuteResult({
         execution_count: this.executionCount,
         data: {
